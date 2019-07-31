4d27a2524ee05bd1c975bb4449bb36faddf2df7c
Rafostar <40623528+Rafostar@users.noreply.github.com>
2019-07-31 22:58:02 +0200

    Use gstreamer-recorder server mode output instead of stdout

--- node_scripts/addon.js
+++ node_scripts/addon.js
@@ -1,5 +1,6 @@
+var net = require('net');
 var gstRecorder = require('gstreamer-recorder');
-var recorder = new gstRecorder({ audio: { encoder: 'lamemp3enc' }});
+var recorder = new gstRecorder({ output: 'server', audio: { encoder: 'lamemp3enc' }});
 
 var config;
 var selection;
@@ -12,25 +13,46 @@ exports.handleSelection = function(selectionContents, configContents)
 	var devices = recorder.getAudioDevices(true);
 	var audioSrc = 'cast_to_tv.monitor';
 
-	if(devices.includes(audioSrc)) recorder.opts.audio.device = audioSrc;
-	else recorder.opts.audio.device = null;
+	if(devices.includes(audioSrc))
+		recorder.opts.audio.device = audioSrc;
+	else
+		recorder.opts.audio.device = null;
+
+	recorder.opts.server.port = config.listeningPort + 1;
 }
 
 exports.closeStream = function()
 {
 	config = null;
 	selection = null;
+
+	stopRecording();
 }
 
 exports.fileStream = function(req, res)
 {
-	res.setHeader('Content-Type', 'video/x-matroska');
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Connection', 'keep-alive');
-	res.statusCode = 200;
+	recorder.start((err) =>
+	{
+		if(err) return res.sendStatus(404);
 
-	recorder.start().pipe(res);
-	req.on('close', recorder.stop);
+		var socket = net.createConnection({
+			port: recorder.opts.server.port,
+			host: recorder.opts.server.host
+		});
+
+		var type = (recorder.opts.format === 'matroska') ? 'x-matroska' : 'mp4';
+
+		res.setHeader('Content-Type', `video/${type}`);
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader('Connection', 'keep-alive');
+		res.statusCode = 200;
+
+		socket.pipe(res);
+		socket.on('error', () => stopRecording());
+	});
+
+	req.on('close', () => stopRecording());
+	req.on('end', () => stopRecording());
 }
 
 exports.subsStream = function(req, res)
@@ -41,4 +63,10 @@ exports.subsStream = function(req, res)
 exports.coverStream = function(req, res)
 {
 	res.sendStatus(204);
+}
+
+function stopRecording()
+{
+	if(recorder.process)
+		recorder.stop();
 }
