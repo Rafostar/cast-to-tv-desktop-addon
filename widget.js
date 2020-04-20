afe2d19ccaaff10508fd938598e1baa23500e584
Rafostar <40623528+Rafostar@users.noreply.github.com>
2020-04-20 17:29:25 +0200

    Use better menu icon when theme lacks preferred one

--- widget.js
+++ widget.js
@@ -1,16 +1,24 @@
-const { GObject } = imports.gi;
+imports.gi.versions.Gtk = '3.0';
+
+const { Gtk, GObject } = imports.gi;
 const PopupMenu = imports.ui.popupMenu;
 const Local = imports.misc.extensionUtils.getCurrentExtension();
 const Gettext = imports.gettext.domain(Local.metadata['gettext-domain']);
 const { CastDesktopRecorder } = Local.imports.recorder;
 const _ = Gettext.gettext;
 
+const REMOTE_DESKTOP_ICON = 'preferences-desktop-remote-desktop-symbolic';
+const FALLBACK_ICON = 'video-display-symbolic';
+
 var AddonMenuItem = GObject.registerClass(
 class CastDesktopMenuItem extends PopupMenu.PopupImageMenuItem
 {
 	_init(mainImports)
 	{
-		super._init(_("Desktop"), 'preferences-desktop-remote-desktop-symbolic');
+		let iconName = (Gtk.IconTheme.get_default().has_icon(REMOTE_DESKTOP_ICON)) ?
+			REMOTE_DESKTOP_ICON : FALLBACK_ICON;
+
+		super._init(_("Desktop"), iconName);
 		this.isDesktopStream = true;
 
 		this.castRecorder = new CastDesktopRecorder(mainImports);
